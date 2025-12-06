6730a0f8f04ac7c2ba30508fea405f9391141b52
KATTA-00 <kanishkagunawarthana@gmail.com>
2025-12-06 22:50:54 +0530

    Add

--- code/power/cpu/src_detailed.f
+++ code/power/cpu/src_detailed.f
@@ -3,36 +3,5 @@
 # Main testbench
 cpu/cpu_tb.v
 
-# Main CPU module  
+# Main CPU module (includes all submodules via `include statements)
 cpu/cpu.v
-
-# ALU and FPU
-alu/alu.v
-fpu/fpu.v
-# Note: fpu.v includes all other FPU files via `include statements
-
-# Register Files
-reg_file/reg_file.v
-f_reg_file/f_reg_file.v
-
-# Control Units
-control_unit/control_unit.v
-branch_control_unit/branch_control_unit.v
-immediate_generation_unit/immediate_generation_unit.v
-hazard_detection_unit/hazard_detection_unit.v
-pipeline_flush_unit/pipeline_flush_unit.v
-
-# Forwarding Units
-forwarding_units/ex_forward_unit.v
-forwarding_units/mem_forward_unit.v
-
-# Pipeline Registers
-pipeline_registers/pr_if_id.v
-pipeline_registers/pr_id_ex.v
-pipeline_registers/pr_ex_mem.v
-pipeline_registers/pr_mem_wb.v
-
-# Support Modules
-support_modules/plus_4_adder.v
-support_modules/mux_2to1_32bit.v
-support_modules/mux_4to1_32bit.v
