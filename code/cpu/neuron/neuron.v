7d7c3c6865841d5cf99274c7d38a79378c55cef1
KATTA-00 <kanishkagunawarthana@gmail.com>
2025-12-07 19:47:29 +0530

    Add

--- code/cpu/neuron/neuron.v
+++ code/cpu/neuron/neuron.v
@@ -1,5 +1,5 @@
 `include "fpu/Multiplication.v"
-`include "fpu/Addition-Subtraction.v"
+`include "fpu/Addition_Subtraction.v"
 `include "fpu/Comparison.v"
 
 `timescale 1ns/100ps
@@ -61,7 +61,12 @@ module neuron (CLK, RESET, SPIKED, I, a, b, c, d);
         if (RESET)
         begin
             V <= #1 c;
-            U <= #1 U_PLUS_D;
+            U <= #1 b;  // Initialize U to b parameter (typical initialization)
+        end
+        else if (SPIKED)
+        begin
+            V <= #1 c;         // Reset V to c when spiked
+            U <= #1 U_PLUS_D;  // Add d to U when spiked
         end
         else
         begin
